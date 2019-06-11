40a3bfc572ec35effe97708b36863b6112f12586
brand222 <Brandon_mcDonald22@yahoo.com>
2019-06-10 21:17:35 -0500

    added stuff on datetime

---
+++ SyntaxBasics/DateTimePractice.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SyntaxBasics
+{
+    class DateTimePractice
+    { 
+        static void Main(string[] args)
+        {
+            //this prints the current date/time
+            Console.WriteLine("Current date/time is : " + DateTime.Now);
+            //create a variable which stores your birthday
+            var birthday = new DateTime(1990, 04, 16);
+            // here we create a variable which stores the 
+            //difference between today's date and your birthday
+            var difference = DateTime.Now - birthday;
+            Console.WriteLine(difference);
+
+            //create an new datetime
+            var someTime = new DateTime(2017, 10, 18, 9, 51, 32);
+            //this adds minutes to the time to go into the future
+           Console.WriteLine(someTime.AddMinutes(134));
+            //adding days, hours, miliseconds etc...
+            // you can also subtract time...
+
+            someTime.AddMinutes(-30);
+
+        }
+    }
+}
---
+++ SyntaxBasics/EnumsAndConstants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SyntaxBasics
+{
+    class EnumsAndConstants
+    {
+        //static void Main(string[] args)
+        //{
+        //    //constants NEVER change
+        //    const float pi = 3.14f;
+        //}
+        ////here is an enum
+        //enum weekDays { Monday = 1, Tuesday = 2, Wendesday = 3, Thursday = 4 };
+
+
+    }
+}
