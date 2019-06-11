ba7feed821cf312e27fe0f04ab7208b5baa6c78c
brand222 <Brandon_mcDonald22@yahoo.com>
2019-06-10 19:59:02 -0500

    Added new program

---
+++ SyntaxBasics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SyntaxBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
