70a955ddf3124a1a7f7d1f6fa9c719eae7b7bd6c
brand222 <Brandon_mcDonald22@yahoo.com>
2019-06-10 20:01:01 -0500

    simple hello world program

--- SyntaxBasics/Program.cs
+++ SyntaxBasics/Program.cs
@@ -4,8 +4,11 @@ namespace SyntaxBasics
 {
     class Program
     {
+        //here is the main method
         static void Main(string[] args)
         {
+            //here is a simple console writing command
+            //'cw' + TAB is the hot-key for this particular command
             Console.WriteLine("Hello World!");
         }
     }
